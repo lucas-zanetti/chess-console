414cea6f77b234d69c6e01c33da32648f6692288
lucas-zanetti <lucas.hzanetti@gmail.com>
2022-08-26 15:41:27 -0300

    Adding class Position

--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace chess_console
+﻿using board;
+
+namespace chess_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Position position = new Position(3, 4);
+
+            Console.WriteLine(position);
         }
     }
 }
